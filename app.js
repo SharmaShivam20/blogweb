76408388b7d4ae50f9d87aeafd8c607ee0ff37b8
Shivam Sharma <shivampurohitt2000@gmail.com>
2025-05-15 18:40:32 +0530

    Updated app.js and other project files

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use(express.static("public"));
 mongoose.set("strictQuery", true);
 
 //Your MongoDB connection string here.
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true,
+  useUnifiedTopology: true});
 
 const postSchema = new mongoose.Schema({
   title:String,
